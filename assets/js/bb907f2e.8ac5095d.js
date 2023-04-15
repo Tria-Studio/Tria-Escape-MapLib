186c7a46744d6810eb66bb4938c0a6e6b8831d80
GlowingUmbreon <umbreon@disroot.org>
2023-04-15 14:14:25 +0100

    Built and published by Moonwave

---
+++ assets/js/bb907f2e.8ac5095d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[468],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/MapLib","label":"\u200bMap\u200bLib"},{"type":"link","href":"/api/PlayerUI","label":"\u200bPlayer\u200bU\u200bI"},{"type":"link","href":"/api/Settings","label":"\u200bSettings"},{"type":"link","href":"/api/Skills","label":"\u200bSkills"}]')}}]);
---
+++ assets/js/d6345fde.b21a3446.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[500],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/Tria-Studio/Tria-Escape-MapLib/blob/master","baseUrl":"/Tria-Escape-MapLib/","classOrder":[],"apiCategories":[]}')}}]);
