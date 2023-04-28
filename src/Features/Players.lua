fdb708d936722859eca0b40ce9246c1988b5825e
Ethan76167 <72885654+Ethan76167@users.noreply.github.com>
2023-04-28 04:16:11 +0100

    Update Players.lua

--- src/Features/Players.lua
+++ src/Features/Players.lua
@@ -1,6 +1,41 @@
--- This script is getting deleted soon
-return {
-	GetPlayers = function()
-		return require(game:GetService("ReplicatedStorage").shared.PlayerStates):GetPlayersWithState(1)
-	end,
-}
+-- Copyright (C) 2023 Tria
+-- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+-- If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+--< Services >--
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local PlayerStates = require(ReplicatedStorage.shared.PlayerStates)
+
+--< Main >--
+local Players = {}
+Players.__index = Players
+
+function Players.new()
+	local self = setmetatable({}, Players)
+	return self
+end
+
+--- Description
+function Players:GetPlayers()
+	return PlayerStates:GetPlayersWithState(PlayerStates.GAME)
+end
+
+--- Description
+--0.11 Feature
+function Players:GetPlayersInRadius(position: Vector3, radius: number): { Player }
+	local ret = {}
+
+	for _, plr in pairs(self:GetPlayers()) do
+		if plr and plr.Character then
+			local rootPart = plr.Character:FindFirstChild("HumanoidRootPart")
+			if rootPart and ((rootPart.Position - position).Magnitude <= radius) then
+				table.insert(ret, plr)
+			end
+		end
+	end
+
+	return ret
+end
+
+return Players
