39130a66aff258578af1c5e1228feeaa4bc06b6f
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-30 23:35:13 +0700

    favicon fix attempts

--- docusaurus.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    title: 'TRIA.os Scripting Documentation',
-    url: 'https://coderheck.github.io',
-    baseUrl: '/maplib-open-source',
-    favicon: '/img/TRIAos-Logo.ico',
-  };
