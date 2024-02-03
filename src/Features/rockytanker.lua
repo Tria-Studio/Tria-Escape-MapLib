047c5380ea350db68446fc994b58509628e87b15
coderheck <loxzyioxzybusiness@hotmail.com>
2024-02-03 21:39:14 +0700

    Merge branch 'master' of https://github.com/coderheck/maplib-open-source

---
+++ src/Features/rockytanker.lua
@@ -0,0 +1,26 @@
+--- @class rockytanker
+--- This is the documentation on rockytanker, one of the documentation writers.
+
+local rockytanker: Types.rockytanker = {}
+rockytanker.__index = rockytanker
+local rockyt = "the racist god."
+
+--- @param stringToWrite string
+--- @within rockytanker
+function rockytanker:Write(stringToWrite: string)
+    if stringToWrite ~= nil then print(stringToWrite) else return nil
+end
+local random = math.random(1, 3)
+print(random)
+print(rocky)
+print("goofy ahh rocky")
+print("embot is racist, noted")
+
+local pi = math.pi
+print("the digits of pi are " .. pi)
+
+--goofy ahh rockytanker
+--webhook doesnt work :skull:
+--okay another webhook test
+--IT WORKS :longcrazyvn_thumbsup:
+end
